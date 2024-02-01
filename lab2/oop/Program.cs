34d38cd7a819cb86cbe29a3558fb7a43bc778da5
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-02-01 18:27:37 +0700

    lab2

---
+++ lab2/oop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace oop
+{
+    //Program.cs : Program class (to run program)
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //object: class instance
+            //declare new Student objects with constructor
+            Student student1 = new Student(12345, "Tuan Hung", "tuanhung.vnu@gmail.com");
+            Student student2 = new Student(12346, "Hung Lam", "hunglam.vnu@gmail.com");
+            Student student3 = new Student(12347, "Manh Hai", "manhhai.vnu@gmail.com");
+
+            //display student 1 information
+            Console.WriteLine("Name of student 1: " + student1.getName());
+            Console.WriteLine("Current email of student 1: " + student1.getEmail());
+            //change email address of student 1
+            student1.setEmail("tuanhung_vnuis@gmail.com");
+            //display new email address of student 1
+            Console.WriteLine("New email of student 1: " + student1.getEmail());
+
+            //display student 2 & 3 information
+            Console.WriteLine("Student 2 Info: ");
+            student2.showStudentInfo();
+            Console.WriteLine("Student 3 Info: ");
+            student3.showStudentInfo();
+        }
+    }
+}
---
+++ lab2/oop/Student.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    //OOP : Object Oriented Programming
+
+    //Student.cs : OOP class
+    internal class Student
+    {
+        //attributes (variables)
+        //always set access modifier for all attributes to private
+        // => information hiding (security)
+        private int Id;
+        private string Name;
+        private string Email;
+
+        //behaviours (methods)
+        //set access modifier to public for the methods you want to share for other classes
+        //constructor
+        public Student () { }  //empty constructor
+        //public Student(int Id, string Name, string Email)
+        //{
+        //    //left-side : variables
+        //    //right-side: parameters
+        //    this.Id = Id;
+        //    this.Name = Name;
+        //    this.Email = Email;
+        //}
+        public Student(int id, string name, string email)
+        {
+            //variable's name and parameter's name are similar => use "this" keyword
+            //variable's name and parameter's name are different => no need to use "this" keyword
+            //C# is case-sensitive language (upper case != lower case)
+            Id = id;
+            Name = name;
+            Email = email;
+        }
+
+        //getter (observer)
+        public int getId()
+        {
+            return this.Id;
+        }
+
+        public string getName()
+        {
+            return this.Name;
+        }
+
+        public string getEmail()
+        {
+            return this.Email;
+        }
+
+        //setter (mutator)
+        public void setId(int Id) {
+            this.Id = Id;
+        }
+        public void setName(string Name) { 
+            this.Name = Name;
+        }
+        public void setEmail(string Email) { 
+            this.Email = Email;
+        }
+
+        //extra methods
+        public void showStudentInfo()
+        {
+            Console.WriteLine("ID: " + this.Id);
+            Console.WriteLine("Name: " + this.Name);
+            Console.WriteLine("Email: " + this.Email);
+        }  
+    }
+}

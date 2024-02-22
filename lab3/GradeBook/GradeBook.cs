e520582796e67527c9b72b36e159a7b61303d335
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-02-22 18:26:20 +0700

    lab3

---
+++ lab3/GradeBook/GradeBook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    internal class GradeBook
+    {
+        //in an OOP class without any constructor
+        //C# will automatic create and use default constructor implicitly (hidden)
+        public GradeBook() { }
+
+        private string name;
+        private double grade;
+
+        public GradeBook(string name, double grade)
+        {
+            this.name = name;
+            this.grade = grade;
+        }
+
+        public void displayMessage()
+        {
+            Console.WriteLine("Welcome to GradeBook app");
+        }
+
+        public void displayGrade()
+        {
+            Console.WriteLine("Student's name: " + name);
+            Console.WriteLine("Student's grade: " + grade);
+        }
+    }
+}
---
+++ lab3/GradeBook/GradeBookTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    internal class GradeBookTest
+    {
+        static void welcome()
+        {
+            Console.WriteLine("welcome to VNU-IS");
+        }
+
+        static void Main(string[] args)
+        {
+            /* if you want to call method in an OOP class
+             * you must create object first */
+            GradeBook gb = new GradeBook();
+            gb.displayMessage();
+
+            /* but if you want to call an internal method
+             * (inside-class method), you don't need to 
+             * create object or anything similar. Just call the method itself by its name */
+            welcome();
+
+            //create new GradeBook object with both "name" and "grade"
+            Console.Write("Enter student's name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter student's grade: ");
+            double grade = Convert.ToDouble(Console.ReadLine());
+            GradeBook gb1 = new GradeBook(name, grade);
+            gb1.displayGrade();
+        }
+    }
+}

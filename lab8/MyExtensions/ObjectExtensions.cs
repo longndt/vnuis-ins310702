d442e2ec4632fe422eb7f1583812fb77dfeb39bd
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-04-01 09:57:48 +0700

    lab8

---
+++ lab8/MyExtensions/ObjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+namespace MyExtensions
+{
+    static class ObjectExtensions
+    {
+        //Define an extension method to System.Object
+        public static void DisplayDefiningAssembly(this object obj)
+        {
+            Console.WriteLine("{0} lives here:\n\t->{1}", obj.GetType().Name, Assembly.GetAssembly(obj.GetType()));
+        }
+    }
+}
---
+++ lab8/MyExtensions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MyExtensions
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Since everything extends System.Object, all classes and structures can use this
+            int myInt = 12345678;
+            myInt.DisplayDefiningAssembly();
+        }
+    }
+}

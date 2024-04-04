09a0958433562a631affeca24a72669dc0e72652
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-04-04 10:43:10 +0700

    lab8

---
+++ lab8/StudentMark/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentMark
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>
+                {
+                    new Student { Name = "Alice", Mark = 90 },
+                    new Student { Name = "Bob", Mark = 80 },
+                    new Student { Name = "Charlie", Mark = 85 },
+                    new Student { Name = "David", Mark = 92 }
+                };
+
+            var highScorer = from student in students
+                             where student.Mark > 85
+                             select student;
+
+            foreach (var student in highScorer)
+            {
+                Console.WriteLine(student.Name + " scored " + student.Mark);
+            }
+        }
+    }
+}
---
+++ lab8/StudentMark/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentMark
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Mark { get; set; }
+    }
+}

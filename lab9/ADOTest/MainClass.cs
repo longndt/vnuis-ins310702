ae93e9cd7b791b025a9b1f7f55921f9c7b70d47f
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-04-09 08:55:36 +0700

    lab9

--- lab9/ADOTest/MainClass.cs
+++ lab9/ADOTest/MainClass.cs
@@ -12,14 +12,18 @@ namespace ADOTest
             {
                 //Establish the connection between .NET and SQL Server with ADO.NET using 1 of 2 below methods
 
-                //1st method: use SQL Server Authentication
-                //Note 1: Replace the placeholder values with your actual server name, database name, user ID, and password
-                //Note 2: Default database username (userid) is "sa" and password has been set from installation step => reset pass if your forgot
+                /* 
+                1st method: use SQL Server Authentication
+                Note 1: Replace the placeholder values with your actual server name, database name, user ID, and password
+                Note 2: Default database username (userid) is "sa" and password has been set from installation step => reset pass if your forgot
+                */
                 //using (SqlConnection connection = new SqlConnection("Server=localhost;Database=AutoLot;User Id=sa;Password=123456;"))
 
-                //2nd method: use Windows Authentication
-                //Note 1: Replace the placeholder values with your actual server name, database name
-                //Note: use "Integrated Security=True;"
+                /*
+                2nd method: use Windows Authentication
+                Note 1: Replace the placeholder values with your actual server name, database name
+                Note 2: use "Integrated Security=True;"
+                */
                 using (SqlConnection connection = new SqlConnection("Server=LG-GRAM;Database=AutoLot;Trusted_Connection=True;"))
                 {
                     // Open connection

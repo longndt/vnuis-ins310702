85c0aaa522cb5860933b7bfdc37fd2e9f94b5cfe
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-04-16 14:13:32 +0700

    lab10

---
+++ lab10/DataAdapterWithLINQ/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace DataAdapterWithLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare database connection string 
+            string db_connection = $"Server=LG-GRAM;Database=AutoLot;Trusted_Connection=True;";
+            //Declare new DataSet
+            DataSet ds = new DataSet();
+            //Declare SQL query
+            string sql_query = "SELECT * FROM Inventory";
+            //Declare new DataAdapter
+            DbDataAdapter adapter = new SqlDataAdapter(sql_query, db_connection);
+
+            //A) LINQ over DataTable (1)
+            adapter.Fill(ds, "Inventory");
+            DataTable tbl = ds.Tables[0];
+            var subset = from car in tbl.AsEnumerable()
+                         where ((string)car["Color"]).Equals("Black")
+                         select car;
+            foreach (var item in subset)
+            {
+                Console.WriteLine($"{item["PetName"]} is {item["Color"]}");
+            }
+ 
+            //B) LINQ over DataTable (2)
+            //adapter.Fill(ds, "Inventory");
+            //DataTable tbl = ds.Tables[0];
+            //var cars = from car in tbl.AsEnumerable()
+            //           where car.Field<string>("Color") == "Black"
+            //           select new
+            //           {
+            //               ID = car.Field<int>("CarID"),
+            //               Make = car.Field<string>("Make"),
+            //               Color = car.Field<string>("Color")
+            //           };
+            //foreach (var item in cars)
+            //{
+            //    Console.WriteLine($"Car {item.ID} is a {item.Color} {item.Make}");
+            //}   
+
+            //C) Hydrating New DataTables from LINQ Queries
+            //adapter.Fill(ds, "Inventory");
+            //DataTable tbl = ds.Tables[0];
+            //var cars = from car in tbl.AsEnumerable()
+            //           where car.Field<string>("Color") == "Black"
+            //           select car;
+            //DataTable newTbl = cars.CopyToDataTable();
+            //foreach (DataRow row in newTbl.Rows)
+            //{
+            //    foreach (DataColumn col in newTbl.Columns)
+            //    {
+            //        Console.Write($"{row[col]}\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+        }
+    }
+}
---
+++ lab10/DataAdapterWithoutLINQ/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace SimpleDataAdapter
+{
+    internal class Program
+    {
+        public static void PrintDataSet(DataSet ds)
+        {
+            //Set DataSet name
+            ds.DataSetName = "AutoLot DataSet";
+            Console.WriteLine($"DataSet Name: {ds.DataSetName}");
+            foreach (DictionaryEntry item in ds.ExtendedProperties)
+            {
+                Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+            }
+            //Write the tables
+            foreach (DataTable tbl in ds.Tables)
+            {
+                Console.WriteLine($"Table name: {tbl.TableName}");
+                foreach (DataColumn col in tbl.Columns)
+                {
+                    Console.Write($"{col.ColumnName}\t");
+                }
+                Console.WriteLine();
+                foreach (DataRow row in tbl.Rows)
+                {
+                    foreach (DataColumn col in tbl.Columns)
+                    {
+                        Console.Write($"{row[col]}\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        static void Main(String[] args)
+        {
+            //Declare database connection string 
+            string db_connection = $"Server=LG-GRAM;Database=AutoLot;Trusted_Connection=True;";
+            //Declare new DataSet
+            DataSet ds = new DataSet();
+            //Declare SQL query
+            string sql_query = "SELECT * FROM Inventory";
+            //Declare new DataAdapter
+            DbDataAdapter adapter = new SqlDataAdapter(sql_query, db_connection);
+
+            //Mapping Database Names to Friendly Names
+            adapter.TableMappings.Add("Inventory", "Demo Inventory");
+            DataTableMapping tablemapping = adapter.TableMappings["Inventory"];
+            tablemapping.ColumnMappings.Add("CarId", "Car Id");
+            tablemapping.ColumnMappings.Add("PetName", "Car Name");
+
+            //Print DataSet
+            adapter.Fill(ds, "Inventory");
+            PrintDataSet(ds);
+        }
+    }
+}
